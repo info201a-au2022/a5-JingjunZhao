59cfb3d88dbe916fcd202237be6a2101bcb40863
Becky Zhao <xiaobezhao@MacBook-Air-72.localdomain>
2022-12-09 12:58:52 -0800

    <xlarge>

--- A5ShinyApp/ui.R
+++ A5ShinyApp/ui.R
@@ -12,6 +12,7 @@ library(maps)
 library(plotly)
 library(rsconnect)
 library(htmltools)
+rsconnect::configureApp("APPNAME", size="xlarge")
 library(shinythemes)
 ?owid<-read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-JingjunZhao/main/owid-co2-data.csv")
 
